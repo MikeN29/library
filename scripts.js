f3e9e105a5814a8e822c441939ea0f22d570f87a
Michael Norman <mikenorman192@msn.com>
2023-02-07 15:50:20 +0000

    add book and display books works

--- scripts.js
+++ scripts.js
@@ -39,13 +39,83 @@ displayBooksButton.addEventListener(
     // Write a function that loops through the array and displays each book on the page.
     // You can display them in some sort of table, or each on their own “card”.
     // It might help for now to manually add a few books to your array so you can see the display.
+    /*
     let container = document.getElementById("displayBooksDiv");
 
-    let bookTable = document.createElement("TABLE");
+    let bookCard = document.createElement("div");
 
-    bookTable.setAttribute("id", "bookTable");
+    bookCard.setAttribute("id", "bookCard");
+    let bookCardID = document.getElementById("bookCard");
 
-    container.appendChild(bookTable);
+    container.appendChild(bookCard);
+
+    bookCard.classList.add("bookCard");
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.innerText = mylibrary[0].title;
+
+    const bookAuthor = document.createElement("h3");
+    bookAuthor.innerText = mylibrary[0].author;
+
+    const bookPages = document.createElement("h3");
+    bookPages.innerText = mylibrary[0].pages;
+
+    const bookRead = document.createElement("h3");
+    bookRead.innerText = mylibrary[0].isRead;
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+
+    */
+    //the below works - i need to change it slightly so that a new div card is created each time
+    for (var i = 0; i < mylibrary.length; i++) {
+      // let container = document.getElementById("displayBooksDiv");
+      // let bookCardID = document.getElementById("bookCard");
+      // bookCard.classList.add("bookCard");
+      // const bookTitle = document.createElement("h3");
+      // bookTitle.innerText = mylibrary[i].title;
+      // const bookAuthor = document.createElement("h3");
+      // bookAuthor.innerText = mylibrary[i].author;
+      // const bookPages = document.createElement("h3");
+      // bookPages.innerText = mylibrary[i].pages;
+      // const bookRead = document.createElement("h3");
+      // bookRead.innerText = mylibrary[i].isRead;
+      // bookCardID.appendChild(bookTitle);
+      // bookCardID.appendChild(bookAuthor);
+      // bookCardID.appendChild(bookPages);
+      // bookCardID.appendChild(bookRead);
+
+      let container = document.getElementById("displayBooksDiv");
+
+      let bookCard = document.createElement("div");
+
+      // bookCard.setAttribute("id", "bookCard");
+      // let bookCardID = document.getElementById("bookCard");
+
+      container.appendChild(bookCard);
+
+      bookCard.classList.add("bookCard");
+
+      const bookTitle = document.createElement("h3");
+      bookTitle.classList.add("bookTitleText");
+      bookTitle.innerText = mylibrary[i].title;
+
+      const bookAuthor = document.createElement("h3");
+      bookAuthor.innerText = mylibrary[i].author;
+
+      const bookPages = document.createElement("h3");
+      bookPages.innerText = mylibrary[i].pages + " pages";
+
+      const bookRead = document.createElement("h3");
+      bookRead.innerText = "Has this book been read? " + mylibrary[i].isRead;
+
+      bookCard.appendChild(bookTitle);
+      bookCard.appendChild(bookAuthor);
+      bookCard.appendChild(bookPages);
+      bookCard.appendChild(bookRead);
+    }
   },
   false
 );
